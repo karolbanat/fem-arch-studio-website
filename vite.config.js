e66bc5b57c9693a543c9091235baf4aa617bac5e
Karol Banat <karol.banat@gmail.com>
2023-03-12 21:07:43 +0100

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, 'index.html'),
+				about: resolve(__dirname, '/pages/about.html'),
+				portfolio: resolve(__dirname, '/pages/portfolio.html'),
+				contact: resolve(__dirname, '/pages/contact.html'),
+			},
+		},
+	},
+});
