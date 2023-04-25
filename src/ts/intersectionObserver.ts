ca0cff317e1ee2d6e2dfb445e1365f431c8d790b
Karol Banat <karol.banat@gmail.com>
2023-04-25 22:06:03 +0200

    Return types

--- src/ts/intersectionObserver.ts
+++ src/ts/intersectionObserver.ts
@@ -1,9 +1,9 @@
 import { animateElement } from './DOM';
 
-const headingsObserver = new IntersectionObserver(headingObserverCallback);
-const projectCardsObserver = new IntersectionObserver(projectCardsObserverCallback);
+const headingsObserver: IntersectionObserver = new IntersectionObserver(headingObserverCallback);
+const projectCardsObserver: IntersectionObserver = new IntersectionObserver(projectCardsObserverCallback);
 
-function headingObserverCallback(entries: IntersectionObserverEntry[], observer: IntersectionObserver) {
+function headingObserverCallback(entries: IntersectionObserverEntry[], observer: IntersectionObserver): void {
 	entries.forEach(entry => {
 		if (entry.isIntersecting) {
 			animateElement(entry.target as HTMLElement, 'animation-slide-fade-down');
@@ -12,7 +12,7 @@ function headingObserverCallback(entries: IntersectionObserverEntry[], observer:
 	});
 }
 
-function projectCardsObserverCallback(entries: IntersectionObserverEntry[], observer: IntersectionObserver) {
+function projectCardsObserverCallback(entries: IntersectionObserverEntry[], observer: IntersectionObserver): void {
 	entries.forEach(entry => {
 		if (entry.isIntersecting) {
 			animateElement(entry.target as HTMLElement, 'animation-fade-pop-up');
@@ -21,7 +21,7 @@ function projectCardsObserverCallback(entries: IntersectionObserverEntry[], obse
 	});
 }
 
-export function initializeObservers() {
+export function initializeObservers(): void {
 	document.querySelectorAll('h2').forEach(heading => headingsObserver.observe(heading));
 	document.querySelectorAll('.project-card').forEach(card => projectCardsObserver.observe(card));
 }
